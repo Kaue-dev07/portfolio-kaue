85ce8ba26cdf3ac5a5b5af8dee2f2c8014fe3c0d
Kauê Rosa <kaue_rosa150@estudante.sesisenai.org.br>
2025-07-09 11:02:07 -0300

    Update script.js

--- aula2/script.js
+++ aula2/script.js
@@ -1,13 +1,20 @@
 const abreHabilidades = document.getElementById('abreHabilidades');
 const habilidadesContainer = document.querySelector('.habilidades-container');
+const abreEstudos = document.getElementById('abreEstudos');
+const estudosContainer = document.querySelector('.estudos-container');
 
 abreHabilidades.onclick = () => {
-    if
-    (habilidadesContainer.style.display === 'block'){
-    habilidadesContainer.style.display = 'none';
-    abreHabilidades.textContent = 'SKILLS';
-  } else {
     habilidadesContainer.style.display = 'block';
-    abreHabilidades.textContent = 'SKILLS';
-  }
+    estudosContainer.style.display = 'none'; 
+
+    abreHabilidades.classList.add('ativo');
+    abreEstudos.classList.remove('ativo');
+};
+
+abreEstudos.onclick = () => {
+    estudosContainer.style.display = 'block';
+    habilidadesContainer.style.display = 'none';
+
+    abreEstudos.classList.add('ativo');
+    abreHabilidades.classList.remove('ativo');
 };
