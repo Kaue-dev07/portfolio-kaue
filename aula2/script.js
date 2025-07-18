06994038332c367d563cd79f4f58b6a9993b7763
Kauê Rosa <kaue_rosa150@estudante.sesisenai.org.br>
2025-07-18 19:32:55 -0300

    Update script.js

--- aula2/script.js
+++ aula2/script.js
@@ -26,3 +26,16 @@ abreEstudos.onclick = () => {
     botaoHabilidades.classList.remove('selecionado');
     botaoEstudos.classList.add('selecionado');
 };
+
+let contador = 1;
+
+setInterval(() => {
+  const slide = document.getElementById('slide' + contador);
+  if (slide) {
+    slide.checked = true;
+    contador++;
+    if (contador > 3) {
+      contador = 1;
+    }
+  }
+}, 4000);
