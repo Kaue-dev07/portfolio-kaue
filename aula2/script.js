21f3b62006563e2f51d7de6b4ba13b46c3b8ec77
Kauê Rosa <kaue_rosa150@estudante.sesisenai.org.br>
2025-08-01 23:05:08 -0300

    Update script.js

--- aula2/script.js
+++ aula2/script.js
@@ -1,32 +1,3 @@
-const abreHabilidades = document.getElementById('abreHabilidades');
-const habilidadesContainer = document.querySelector('.habilidades-container');
-const abreEstudos = document.getElementById('abreEstudos');
-const estudosContainer = document.querySelector('.estudos-container');
-const botaoHabilidades = document.querySelector('.btnHabilidades');
-const botaoEstudos = document.querySelector('.btnEstudos');
-
-abreHabilidades.onclick = () => {
-    habilidadesContainer.style.display = 'block';
-    estudosContainer.style.display = 'none'; 
-
-    abreHabilidades.classList.add('ativo');
-    abreEstudos.classList.remove('ativo');
-
-    botaoHabilidades.classList.add('selecionado');
-    botaoEstudos.classList.remove('selecionado');
-};
-
-abreEstudos.onclick = () => {
-    estudosContainer.style.display = 'block';
-    habilidadesContainer.style.display = 'none';
-
-    abreEstudos.classList.add('ativo');
-    abreHabilidades.classList.remove('ativo');
-
-    botaoHabilidades.classList.remove('selecionado');
-    botaoEstudos.classList.add('selecionado');
-};
-
 let contador = 1;
 
 setInterval(() => {
